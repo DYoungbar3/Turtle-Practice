ad7a3346dec881393ee542135bff274036211417
Daniel Youngbar <dyoungbar928@gmail.com>
2023-04-04 13:29:17 -0700

    Create simple table using prettytable

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape('turtle')
-timmy.color('DarkSeaGreen')
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.color('DarkSeaGreen')
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
+# my_screen = Screen()
+# print(my_screen.canvheight)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column('Type', ['Electric', 'Water', 'Fire'])
+table.align = 'l'
+
+print(table)
