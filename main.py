8804226b2153dd4458ab5351a094c73d575c0a56
Daniel Youngbar <dyoungbar928@gmail.com>
2023-04-04 13:22:05 -0700

    Simple stuff to work with turtle module

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape('turtle')
+timmy.color('DarkSeaGreen')
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
